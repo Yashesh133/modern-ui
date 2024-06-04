7440aec7bd29d13c7d8b6ff7353a9e7c959c911e
Yashesh133 <yashcreative133@gmail.com>
2024-06-04 13:29:48 +0530

    demo

--- Data/index.tsx
+++ Data/index.tsx
@@ -8,6 +8,16 @@ export const navItems = [
   
   export const gridItems = [
     {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
       id: 2,
       title: "I'm very flexible with time zone communications",
       description: "",
@@ -27,6 +37,17 @@ export const navItems = [
       img: "",
       spareImg: "",
     },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
     {
       id: 5,
       title: "Currently building a JS Animation library",
