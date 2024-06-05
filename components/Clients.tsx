1aed9106c2f9a58dcba7806d1c61ac5e005a71bb
Yashesh133 <yashcreative133@gmail.com>
2024-06-05 17:00:29 +0530

    spell mistake

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -6,7 +6,7 @@ const Clients = () => {
   return (
     <div className="py-20" id="testimonials">
       <h1 className="heading">
-        Kind Words from <span className="text-purple">Satissfied Clients</span>
+        Kind Words from <span className="text-purple">Satisfied Clients</span>
       </h1>
       <div className="flex flex-col items-center max-lg:mt-10">
         <InfiniteMovingCards
